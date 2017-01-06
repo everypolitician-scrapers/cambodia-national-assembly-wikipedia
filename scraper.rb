f6d0f2c98ab4a69cbbbf7eeb700be406324afcee
Tony Bowden <tony@tmtm.com>
2017-01-06 12:27:11 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'pry'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -21,20 +22,20 @@ end
 def scrape_list(url)
   noko = noko_for(url)
   rows = noko.xpath('//table[.//th[contains(.,"MP")]]//tr[td]')
-  raise "No rows" if rows.count.zero?
+  raise 'No rows' if rows.count.zero?
   rows.each do |tr|
     td = tr.css('td')
-    data = { 
-      name: td[1].text.tidy,
-      wikiname: td[1].xpath('.//a[not(@class="new")]/@title').text,
-      party: td[2].text.tidy,
+    data = {
+      name:           td[1].text.tidy,
+      wikiname:       td[1].xpath('.//a[not(@class="new")]/@title').text,
+      party:          td[2].text.tidy,
       party_wikiname: td[2].xpath('.//a[not(@class="new")]/@title').text,
-      area: td[1].xpath('preceding::h3/span[@class="mw-headline"]').last.text,
-      term: 5,
-      source: url,
+      area:           td[1].xpath('preceding::h3/span[@class="mw-headline"]').last.text,
+      term:           5,
+      source:         url,
     }
-    ScraperWiki.save_sqlite([:name, :area, :party, :term], data)
+    ScraperWiki.save_sqlite(%i(name area party term), data)
   end
 end
 
-scrape_list("https://en.wikipedia.org/wiki/List_of_MPs_of_the_National_Assembly_of_Cambodia")
+scrape_list('https://en.wikipedia.org/wiki/List_of_MPs_of_the_National_Assembly_of_Cambodia')
